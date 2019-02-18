5a25f59db13bef446a0cf3b4d65037c12d39fb1d
Mingyuan Shan <mingyuan.shan@rochester.edu>
2019-02-17 22:03:38 -0500

    adding bootstrap

--- assignment04/index.php
+++ assignment04/index.php
@@ -2,7 +2,7 @@
 
 
 <head>
-    
+
     <?php include "inc/head.inc";?>
     <title>Home | Boston | Assignment 04</title>
 
@@ -10,29 +10,32 @@
 
 
 <body>
-
-    <h1>Team Boston Assignment 04 Homepage</h1>
+<div class="container">
+<h1>Team Boston Assignment 04 Homepage</h1>
 
 <?php include "inc/nav.inc";?>
-
-        <a href="amanda.php">
+</div>
+<div class="container">
+    <a href="amanda.php">
         <h2>Amanda O'Donnell</h2>
         <img src="images/amanda.jpg" alt="Amanda">
         <div>Brain and Cognitive Sciences with Computer Science and Psychology Minors '19.</div>
-        </a>
-
-        <a href="jacob.php">
+    </a>
+</div>
+<div class="container">
+    <a href="jacob.php">
         <h2>Won Kuk Lee</h2>
         <img src="images/wonkuk-headshot.jpg" alt="Won Kuk">
         <div>Computer Science '19.</div>
-        </a>
-
-        <a href="joseph.php">
+    </a>
+</div>
+<div class="container">
+    <a href="joseph.php">
         <h2>Joseph Hur</h2>
         <img src="images/jhur.jpg" alt="Joseph">
         <div>Computer Science and Anthropology '19.</div>
-        </a>
-
+    </a>
+</div>
 
 <?php include "inc/footer.inc";?>
 <?php include "inc/scripts.inc";?>
--- assignment04/joseph.php
+++ assignment04/joseph.php
@@ -2,10 +2,10 @@
 
 
 <head>
-    
+
     <?php include "inc/head.inc";?>
     <title>Joseph Hur | Boston | Assignment 04</title>
-    
+
 </head>
 
 
@@ -13,12 +13,12 @@
 <body>
 
 
-    <header>
-        <a href="index.php">
+<header class="container">
+    <a href="index.php">
         <h1>Joseph Hur</h1>
         <img src="images/jhur.jpg" alt="Joseph Hur">
-        </a>
-    </header>
+    </a>
+</header>
 
 
 
@@ -27,103 +27,108 @@
 
 
 
-    <main>
+<main>
 
-        <h2>Major and Class Year</h2>
 
-        <div>Computer Science and Anthropology '19.</div>
+    <div class="container">
 
-        <h2>Classes Taken at the University of Rochester</h2>
+    <h2>Major and Class Year</h2>
 
-        <p>
+    <div>Computer Science and Anthropology '19.</div>
 
-        <ul>
+    <h2>Classes Taken at the University of Rochester</h2>
+
+    <p>
+
+    <ul>
         <li>CSC 210</li>
         <li>CSC 261</li>
         <li>CSC 170</li>
         <li>CSC 174</li>
-        </ul>
+    </ul>
 
-        </p>
+    </p>
 
-        <h2>Work Experience</h2>
-        <p>
-            Senior year of
-            high school I had my first computer-related internship, at KAB computers. While the job itself involved
-            little programming, I was able to get a great sense of the type and scope of software various types of
-            businesses need. During that job I shadowed employees as they worked in places like government buildings
-            and large businesses. CSC210 was a course that covered the both the back end and the front
-            end aspects of development, and its final project involved making a website from scratch. My group’s final
-            project was called Smart Scheduler, and was a web app that automated the scheduling of a user’s event. It
-            was a good introduction to the team dynamics of building a website, as well as how different programming
-            let’s say a server versus the user interface. 
-        </p>
+    </div>
 
-
-    </main>
-
+    <h2>Work Experience</h2>
+    <p>
+        Senior year of
+        high school I had my first computer-related internship, at KAB computers. While the job itself involved
+        little programming, I was able to get a great sense of the type and scope of software various types of
+        businesses need. During that job I shadowed employees as they worked in places like government buildings
+        and large businesses. CSC210 was a course that covered the both the back end and the front
+        end aspects of development, and its final project involved making a website from scratch. My group’s final
+        project was called Smart Scheduler, and was a web app that automated the scheduling of a user’s event. It
+        was a good introduction to the team dynamics of building a website, as well as how different programming
+        let’s say a server versus the user interface.
+    </p>
 
 
-
-    <aside>
-
-        <h2>Interests and Hobbies</h2>
-
-        <p>I was born in Korea and spent most of my childhood in underdeveloped countries such
-            as the Papua New Guinea and the Philippines. So when I came to the United States in elementary school, I
-            was fascinated by the prevalence of computers and their sheer power. All I thought about was the next time
-            I could go on a computer and start making stick fights using this old program called Scratch. I think it
-            was this stark contrast between being in such underdeveloped countries and coming to the US and seeing all
-            this accessible and cool technology that made me interested in programming and computers in general.
-            </p>
-
-    </aside>
+</main>
 
 
 
-    <!-- <aside>
-        <ul>
-            <li>Student @ University of Rochester</li>
-            <li>Computer Science/Anthropology Major</li>
-            <li>Aspiring Web/Software Developer</li>
-        </ul>
-    </aside>
 
-    <main>
-        <p>
-            I am currently a 4th year student at the University of Rochester studying both Computer Science and
-            Anthropology. I was born in Fort Worth, Texas, but have lived in Hawaii (where one of my sisters was born)
-            and New Jersey (where my other sister was born). I grew up mostly in Bergen County, NJ, especially within
-            the Korean-American enclave of Palisades Park. Growing up I was always fascinated by computers and how they
-            worked. I remember milestones like the process of installing my first graphics card, and learning how to
-            mod video games.This semester, I am taking CSC174 and CSC 261. I hope to learn more advanced concepts of
-            front-end development from 174, and gain a more intricate understanding of relational databases through
-            261.
-        </p>
-        <h2>Early Days</h2>
-        <p>
-            I went to high school at Academes@Englewood in Englewood, New Jersey. I had first interaction with web
-            development, and computer science as a whole, during my freshman year of high school. It was an “Intro to
-            Technology” class, and we made a (very simple) webpage, with only HTML and no CSS. I remember my topic was
-            George Washington Carver, and it was a simple webpage with an image and text, with no styling. Senior of
-            high school I had my first computer-related internship, at KAB computers. While the job itself involved
-            little programming, I was able to get a great sense of the type and scope of software various types of
-            businesses need. During that job I shadowed employees as they worked in places like government buildings
-            and large businesses.
-        </p>
-        <h2>College</h2>
-        <p>
-            During college, I had initially selected Anthropology as my major, but during sophomore I resolved to get a
-            degree in computer science as well. Aside from the core pre-requisites, I had, and am currently taking
-            courses focusing on web development. For example, CSC170 was a great exploration of the basics of front-end
-            web development, such as HTML and CSS, as well as an excellent introduction to some of the more complex
-            ideas, such scripting and web metrics. CSC210 was a course that covered the both the back end and the front
-            end aspects of development, and its final project involved making a website from scratch. My group’s final
-            project was called Smart Scheduler, and was a web app that automated the scheduling of a user’s event. It
-            was a good introduction to the team dynamics of building a website, as well as how different programming
-            let’s say a server versus the user interface. 
-        </p>
-    </main> -->
+<aside>
+
+    <h2>Interests and Hobbies</h2>
+
+    <p>I was born in Korea and spent most of my childhood in underdeveloped countries such
+        as the Papua New Guinea and the Philippines. So when I came to the United States in elementary school, I
+        was fascinated by the prevalence of computers and their sheer power. All I thought about was the next time
+        I could go on a computer and start making stick fights using this old program called Scratch. I think it
+        was this stark contrast between being in such underdeveloped countries and coming to the US and seeing all
+        this accessible and cool technology that made me interested in programming and computers in general.
+    </p>
+
+</aside>
+
+
+
+<!-- <aside>
+    <ul>
+        <li>Student @ University of Rochester</li>
+        <li>Computer Science/Anthropology Major</li>
+        <li>Aspiring Web/Software Developer</li>
+    </ul>
+</aside>
+
+<main>
+    <p>
+        I am currently a 4th year student at the University of Rochester studying both Computer Science and
+        Anthropology. I was born in Fort Worth, Texas, but have lived in Hawaii (where one of my sisters was born)
+        and New Jersey (where my other sister was born). I grew up mostly in Bergen County, NJ, especially within
+        the Korean-American enclave of Palisades Park. Growing up I was always fascinated by computers and how they
+        worked. I remember milestones like the process of installing my first graphics card, and learning how to
+        mod video games.This semester, I am taking CSC174 and CSC 261. I hope to learn more advanced concepts of
+        front-end development from 174, and gain a more intricate understanding of relational databases through
+        261.
+    </p>
+    <h2>Early Days</h2>
+    <p>
+        I went to high school at Academes@Englewood in Englewood, New Jersey. I had first interaction with web
+        development, and computer science as a whole, during my freshman year of high school. It was an “Intro to
+        Technology” class, and we made a (very simple) webpage, with only HTML and no CSS. I remember my topic was
+        George Washington Carver, and it was a simple webpage with an image and text, with no styling. Senior of
+        high school I had my first computer-related internship, at KAB computers. While the job itself involved
+        little programming, I was able to get a great sense of the type and scope of software various types of
+        businesses need. During that job I shadowed employees as they worked in places like government buildings
+        and large businesses.
+    </p>
+    <h2>College</h2>
+    <p>
+        During college, I had initially selected Anthropology as my major, but during sophomore I resolved to get a
+        degree in computer science as well. Aside from the core pre-requisites, I had, and am currently taking
+        courses focusing on web development. For example, CSC170 was a great exploration of the basics of front-end
+        web development, such as HTML and CSS, as well as an excellent introduction to some of the more complex
+        ideas, such scripting and web metrics. CSC210 was a course that covered the both the back end and the front
+        end aspects of development, and its final project involved making a website from scratch. My group’s final
+        project was called Smart Scheduler, and was a web app that automated the scheduling of a user’s event. It
+        was a good introduction to the team dynamics of building a website, as well as how different programming
+        let’s say a server versus the user interface.
+    </p>
+</main> -->
 
 <?php include "inc/footer.inc";?>
 <?php include "inc/scripts.inc";?>
